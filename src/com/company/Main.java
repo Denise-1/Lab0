f950f839f3c5cba18ca44f16a507109384cbc5ab
Denise-1 <denisechenc@gmail.com>
2021-09-20 23:31:27 -0400

    Lab 0

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+// Denise Chen
+// IS 300-03
+// d189@umbc.edu
+// 9/20/2021
+// print out messages
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first message");
+        System.out.println("This is my second message");
+        System.out.println("This is my third message");
+        System.out.println("This is another message");
+        System.out.println("This is another message without error");
+
+    }
+}
